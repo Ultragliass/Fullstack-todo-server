f8d34c15f55c530fadf8bbce8da8433f45a6c0be
Michael H <MichaelHahmigery@gmail.com>
2020-06-06 21:30:47 +0300

    Create server base.

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express, { Errback } from "express";
+import expressJwt from "express-jwt";
+import cors from "cors";
+
+const app = express();
+const PORT: string | number = process.env.PORT || 3001;
+const SECRET: string = process.env.JWT_SECRET || "test";
+
+app.use(express.json());
+app.use(cors());
+
+app.use(
+  expressJwt({ secret: SECRET }).unless({ path: ["/register", "/login"] })
+);
+
+app.all("/", (req, res) => {
+  res.status(404).send("Were you looking for something?");
+});
+
+app.use((err: Errback, req: any, res: any, next: any) => {
+  console.log(err);
+
+  res.status(500).send("Oops! Something went wrong...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}.`);
+});
